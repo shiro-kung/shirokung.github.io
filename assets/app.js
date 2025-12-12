a143ead5a7d4bee802a46abc7d79bedbf3af484f
shiro-kung <jokungzab2109@gmail.com>
2025-12-12 23:19:20 +0700

    Delete assets directory

--- assets/app.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const hamburger = document.getElementById('hamburger');
-  const navLinks = document.getElementById('nav-links');
-
-  hamburger.addEventListener('click', () => {
-    navLinks.classList.toggle('open');
-    hamburger.classList.toggle('active');
-  });
-});
